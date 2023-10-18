a13024c815528095a925a9c393f8cf71e506f7d9
manikvermasdm <manikvermasdm@gmail.com>
2023-10-18 15:08:25 +0530

    native module counter added

---
+++ ios/Counter.swift
@@ -0,0 +1,8 @@
+//
+//  Counter.swift
+//  NativeApp
+//
+//  Created by ManikVerma on 18/10/23.
+//
+
+import Foundation
---
+++ ios/NativeApp-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  NativeApp-Bridging-Header.h
+//  NativeApp
+//
+//  Created by ManikVerma on 18/10/23.
+//
+
+#ifndef NativeApp_Bridging_Header_h
+#define NativeApp_Bridging_Header_h
+
+
+#endif /* NativeApp_Bridging_Header_h */
